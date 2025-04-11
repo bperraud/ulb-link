ee80841a5600fe9701d7bbd8c8b5f5e826198ec8
bperraud <benjamin.perraudin@ulb.be>
2025-04-11 13:05:53 +0200

    Link Model

--- app/permalink/link/models.py
+++ app/permalink/link/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="link")
+    creation_date = models.DateTimeField(auto_now=True)
+    target_url = models.URLField(verbose_name="Target URL")
+    token = models.CharField(max_length=50, verbose_name="Token")
