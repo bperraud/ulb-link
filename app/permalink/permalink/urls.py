d22498a7ed3ba35b839699c0af91d86cfaa9005b
bperraud <benjamin.perraudin@ulb.be>
2025-05-08 12:26:27 +0200

    css left navbar

--- app/permalink/permalink/urls.py
+++ app/permalink/permalink/urls.py
@@ -18,9 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
 
 from link.views import targetURL
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -39,3 +41,6 @@ urlpatterns = [
         name="logout",
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
