a6e9490ce34eb1c5ef2d8b052c79e56ed0092d1e
bperraud <benjamin.perraudin@ulb.be>
2025-05-08 16:22:17 +0200

    form submission success

--- app/permalink/link/views.py
+++ app/permalink/link/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, DetailView, ListView, UpdateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import FormView
+
+from django.urls import reverse_lazy
+
 
 from rest_framework.generics import get_object_or_404
 from link.models import Link, User
+
 
 from link.forms import LinkForm
 
@@ -20,16 +25,36 @@ class LinkListView(ListView):
         return Link.objects.filter(user=self.request.user)
 
 
-@method_decorator([login_required], name="dispatch")
-class LinkEditRowView(TemplateView):
-    # template_name = "form_snippet.html"
-    template_name = "modal.html"
-    # template_name = "edit_row.html"
+# @method_decorator([login_required], name="dispatch")
+# class LinkEditRowView(TemplateView):
+#     template_name = "modal.html"
 
-    def get_context_data(self, **kwargs):
-        link = get_object_or_404(Link, pk=kwargs["pk"])
-        form = LinkForm(instance=link)
-        return {"form": form}
+#     def get_context_data(self, **kwargs):
+#         link = get_object_or_404(Link, pk=kwargs["pk"])
+#         form = LinkForm(instance=link)
+#         return {"form": form}
+
+
+class LinkEditRowView(FormView):
+    template_name = "modal.html"
+    form_class = LinkForm
+
+    def get_success_url(self):
+        return reverse_lazy("link-home", kwargs={"pk": self.kwargs["pk"]})
+
+    def get_initial(self):
+        link = get_object_or_404(Link, pk=self.kwargs["pk"])
+        return {"token": link.token, "target_url": link.target_url}
+
+    def form_valid(self, form):
+        link = get_object_or_404(Link, pk=self.kwargs["pk"])
+        link.token = form.cleaned_data["token"]
+        link.target_url = form.cleaned_data["target_url"]
+        link.save()
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
 
 
 @method_decorator([login_required], name="dispatch")
