e7c35dab9cd23ee3546cf53bd76d28f5e16d1b16
bperraud <74benji@live.fr>
2025-05-09 13:54:26 +0200

    toolbar udpate on checkbox selection

--- app/permalink/link/urls.py
+++ app/permalink/link/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from link.api import ExternalLinkAPIView, LinkAPIView
-from link.views import LinkListView, LinkRowView, edit_link
+from link.views import LinkListView, LinkRowView, edit_link, toolbar
 
 urlpatterns = [
     path("api/create/", ExternalLinkAPIView.as_view(), name="external-api"),
     path("api/<int:pk>", LinkAPIView.as_view(), name="link-api"),
+    path("toolbar/<int:nb>", toolbar),
     path("edit/<int:pk>", edit_link, name="link-edit-row"),
     path("row/<int:pk>", LinkRowView.as_view(), name="link-row"),
     path("", LinkListView.as_view(), name="link-home"),
--- app/permalink/link/views.py
+++ app/permalink/link/views.py
@@ -3,19 +3,13 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import FormView
-
+from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
-from django.urls import reverse_lazy
+from django.urls import reverse
 
 from rest_framework.generics import get_object_or_404
 from link.models import Link
-
-from django.http import JsonResponse, HttpResponse
-
 from link.forms import LinkForm
-from django.urls import reverse
-from django.template.loader import render_to_string
 
 
 @method_decorator([login_required], name="dispatch")
@@ -26,6 +20,26 @@ class LinkListView(ListView):
 
     def get_queryset(self):
         return Link.objects.filter(user=self.request.user)
+
+
+@method_decorator([login_required], name="dispatch")
+class LinkRowView(TemplateView):
+    template_name = "link_row.html"
+
+    def get_context_data(self, **kwargs):
+        link = get_object_or_404(Link, pk=kwargs["pk"])
+        return {"link": link}
+
+
+@require_http_methods(["GET"])
+def toolbar(request, nb):
+    match nb:
+        case 0:
+            return HttpResponse("")
+        case 1:
+            return render(request, "link_bar/edit_single_link.html")
+        case _:
+            return render(request, "link_bar/edit_multiple_link.html")
 
 
 @require_http_methods(["GET", "POST"])
@@ -42,15 +56,6 @@ def edit_link(request, pk):
         form = LinkForm(instance=link)
 
     return render(request, "modal.html", {"form": form})
-
-
-@method_decorator([login_required], name="dispatch")
-class LinkRowView(TemplateView):
-    template_name = "link_row.html"
-
-    def get_context_data(self, **kwargs):
-        link = get_object_or_404(Link, pk=kwargs["pk"])
-        return {"link": link}
 
 
 def targetURL(request, token):
