70c915106b6ba4f254216f9dfe4076cff89b2c8d
bperraud <74benji@live.fr>
2025-05-09 11:42:24 +0200

    form submission with modal

--- app/permalink/link/urls.py
+++ app/permalink/link/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from link.api import ExternalLinkAPIView, LinkAPIView
-from link.views import LinkListView, LinkEditRowView, LinkRowView
+from link.views import LinkListView, LinkRowView, edit_link
 
 urlpatterns = [
     path("api/create/", ExternalLinkAPIView.as_view(), name="external-api"),
     path("api/<int:pk>", LinkAPIView.as_view(), name="link-api"),
-    path("edit/<int:pk>", LinkEditRowView.as_view(), name="link-edit-row"),
+    path("edit/<int:pk>", edit_link, name="link-edit-row"),
     path("row/<int:pk>", LinkRowView.as_view(), name="link-row"),
     path("", LinkListView.as_view(), name="link-home"),
     path("<int:pk>", LinkListView.as_view(), name="link-home"),
--- app/permalink/link/views.py
+++ app/permalink/link/views.py
@@ -1,19 +1,20 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, DetailView, ListView, UpdateView
+from django.views.generic import TemplateView, ListView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import FormView
 
+from django.views.decorators.http import require_http_methods
 from django.urls import reverse_lazy
 
 from rest_framework.generics import get_object_or_404
-from link.models import Link, User
+from link.models import Link
 
-from django.http import HttpResponseBadRequest, JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 from link.forms import LinkForm
-
+from django.urls import reverse
 from django.template.loader import render_to_string
 
 
@@ -27,39 +28,20 @@ class LinkListView(ListView):
         return Link.objects.filter(user=self.request.user)
 
 
-# @method_decorator([login_required], name="dispatch")
-# class LinkEditRowView(TemplateView):
-#     template_name = "modal.html"
+@require_http_methods(["GET", "POST"])
+def edit_link(request, pk):
+    link = get_object_or_404(Link, pk=pk)
+    if request.method == "POST":
+        form = LinkForm(request.POST, instance=link)
+        if form.is_valid():
+            form.save()
+            response = HttpResponse()
+            response["HX-Redirect"] = reverse("link-home")
+            return response
+    else:
+        form = LinkForm(instance=link)
 
-#     def get_context_data(self, **kwargs):
-#         link = get_object_or_404(Link, pk=kwargs["pk"])
-#         form = LinkForm(instance=link)
-#         return {"form": form}
-
-
-class LinkEditRowView(FormView):
-    template_name = "modal.html"
-    form_class = LinkForm
-
-    def get_success_url(self):
-        return reverse_lazy("link-home", kwargs={"pk": self.kwargs["pk"]})
-
-    def get_initial(self):
-        link = get_object_or_404(Link, pk=self.kwargs["pk"])
-        return {"token": link.token, "target_url": link.target_url}
-
-    def form_valid(self, form):
-        link = get_object_or_404(Link, pk=self.kwargs["pk"])
-        link.token = form.cleaned_data["token"]
-        link.target_url = form.cleaned_data["target_url"]
-        link.save()
-        return super().form_valid(form)
-
-    def form_invalid(self, form):
-        html = render_to_string("form_snippet.html", {"form": form})
-        return JsonResponse({"html": html})
-        # return render(self.request, "form_snippet.html", {"form": form})
-        # return super().form_invalid(form)
+    return render(request, "modal.html", {"form": form})
 
 
 @method_decorator([login_required], name="dispatch")
