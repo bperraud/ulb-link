7894bb0818f78e721c6a6c7c467c602f5d0d6a04
bperraud <74benji@live.fr>
2025-05-11 12:29:02 +0200

    disable target_url input in form

--- app/permalink/link/forms.py
+++ app/permalink/link/forms.py
@@ -1,8 +1,21 @@
 from django.forms import ModelForm
 from link.models import Link
+from django import forms
 
 
 class LinkForm(ModelForm):
     class Meta:
         model = Link
         fields = ["token", "target_url"]
+        widgets = {
+            "target_url": forms.TextInput(
+                attrs={
+                    "style": "width: 100%;",
+                    "placeholder": "Enter target URL",
+                }
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["target_url"].disabled = True
