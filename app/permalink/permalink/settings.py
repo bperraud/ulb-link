01350536613acd90fc6895d2361714cfdae6ba9e
bperraud <74benji@live.fr>
2025-11-06 14:21:46 +0100

    fix delete link

--- app/permalink/permalink/settings.py
+++ app/permalink/permalink/settings.py
@@ -157,7 +157,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Europe/Brussels'
 USE_I18N = True
 USE_TZ = True
 
