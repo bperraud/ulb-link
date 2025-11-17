e7a33394b00ae0c462bfe1a657e1b0a56897da34
bperraud <74benji@live.fr>
2025-11-17 13:03:44 +0100

    datatable: search as true

--- app/permalink/link/decorators.py
+++ app/permalink/link/decorators.py
@@ -4,6 +4,7 @@ from functools import wraps
 def nextcloud_user_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
+        return view_func(request, *args, **kwargs)
         if getattr(request.user, "is_nextcloud_user", True):
             return view_func(request, *args, **kwargs)
         return HttpResponseForbidden("You need to log with nextcloud to view this page.")
