3b39a1b1450b4c1097ba821ba766bb579b5b1bc1
bperraud <74benji@live.fr>
2025-11-17 14:04:13 +0100

    input checkbox color adjustment

--- app/permalink/entrypoint.sh
+++ app/permalink/entrypoint.sh
@@ -15,9 +15,8 @@ echo "Collecting statics"
 python manage.py collectstatic --noinput
 
 echo "Building tailwind"
-# python manage.py tailwind install
-# python manage.py tailwind build
+python manage.py tailwind install
+python manage.py tailwind build
 
 ## required for docker to continue
 exec "$@"
-
--- app/permalink/permalink/settings.py
+++ app/permalink/permalink/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     "link",
 ]
 
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
