938307da61e5de1ba2c29c2f3bee535cb73abfb8
bperraud <74benji@live.fr>
2025-11-19 14:18:05 +0100

    django-tailwind-cli package

--- app/permalink/entrypoint.sh
+++ app/permalink/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#echo "Waiting for postgres"
-#while ! nc -z "$DATABASE_HOST" "$DATABASE_PORT" ; do
-#  sleep 0.1
-#done
+echo "Waiting for postgres"
+while ! nc -z "$DATABASE_HOST" "$DATABASE_PORT" ; do
+  sleep 0.1
+done
 
 echo "Postgres started"
 
