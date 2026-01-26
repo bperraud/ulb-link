6c16e332fee475f9685e16f5dfd57551aad92e70
bperraud <74benji@live.fr>
2026-01-26 16:29:57 +0100

    rm OIDC_FORCE_HTTPS

--- app/permalink/link/apps.py
+++ app/permalink/link/apps.py
@@ -1,19 +1,6 @@
 from django.apps import AppConfig
-from django.conf import settings
-import mozilla_django_oidc.utils
 
 
 class PermalinkConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "link"
-
-
-def absolutify_https(request, path):
-    url = request.build_absolute_uri(path)
-    print(url)
-    if settings.OIDC_FORCE_HTTPS:
-        url = url.replace("http://", "https://", 1)
-    return url
-
-
-mozilla_django_oidc.utils.absolutify = absolutify_https
