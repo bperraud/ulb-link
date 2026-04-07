3748b44a98fe74e5676cb991daed9de1e93287f8
bperraud <74benji@live.fr>
2026-04-07 11:23:03 +0200

    remove celery tasks

--- app/permalink/permalink/tasks.py
+++ app/permalink/permalink/tasks.py
@@ -1,9 +1,9 @@
-from celery import shared_task
-from link.models import Link
+# from celery import shared_task
+# from link.models import Link
 
 
-@shared_task
-def validate_url(name="test"):
-    for link in Link.objects.all():
-        if not link.self_test():
-            print("this link doesnt work")
+# @shared_task
+# def validate_url(name="test"):
+#     for link in Link.objects.all():
+#         if not link.self_test():
+#             print("this link doesnt work")
