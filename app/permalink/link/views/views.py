fca1d5f72dd0fef053855fded850dde2be6ba89e
bperraud <74benji@live.fr>
2026-04-09 14:03:18 +0200

    remove generate_token

--- app/permalink/link/views/views.py
+++ app/permalink/link/views/views.py
@@ -120,6 +120,7 @@ def create_bulk(request):
     if request.method == "POST":
         selected_ids = request.POST.getlist("share_checkbox")
         for share in shares:
+            print(share)
             if share["id"] not in selected_ids:
                 continue
             share = Share.objects.create(uid=share["id"], target_url=share["url"])
