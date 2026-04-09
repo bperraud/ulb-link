d068d5bc62c87eec82acc358b72e834495b22fa4
bperraud <74benji@live.fr>
2026-04-09 12:03:27 +0200

    exclude internal shares

--- app/permalink/link/views/views.py
+++ app/permalink/link/views/views.py
@@ -120,7 +120,7 @@ def create_bulk(request):
     if request.method == "POST":
         selected_ids = request.POST.getlist("share_checkbox")
         for share in shares:
-            if share["id"] not in selected_ids:
+            if share["id"] not in selected_ids or share["share_type"] != 3:
                 continue
             share = Share.objects.create(uid=share["id"], target_url=share["url"])
             Link.objects.create(user=request.user, share=share)
